4d6267e06d2927518cfcc1cd2aaea0de44d38c2e
smnlrn <simonlaurin2yahoo.com>
2019-11-19 20:14:37 -0500

    Player name logic, but it is getting messy...

--- bracket_scorer_main.py
+++ bracket_scorer_main.py
@@ -12,9 +12,9 @@ FPS = 30  # frames per second, the general speed of the program
 WINDOWWIDTH = 800  # size of window's width in pixels
 WINDOWHEIGHT = 480  # size of windows' height in pixels
 
-JVT = .9 #Joystick value treshold
+JVT = .9  # Joystick Value Threshold
 
-#            R    G    B
+#       R    G    B
 BLACK = (0, 0, 0)
 GRAY = (100, 100, 100)
 NAVYBLUE = (60, 60, 100)
@@ -26,6 +26,28 @@ YELLOW = (255, 255, 0)
 ORANGE = (255, 128, 0)
 PURPLE = (255, 0, 255)
 CYAN = (0, 255, 255)
+
+# Player Info Page Variables
+letterGap = 50
+letterMarginLeft = 500
+letterMarginTop = 180
+squareSide = 40
+squareAlignement = 5
+
+switch = 0
+letters = [["A", "B", "C", "D", "E", "F"], ["G", "H", "I", "J", "K", "L"], ["M", "N", "O", "P", "Q", "R"],
+           ["S", "T", "U", "V", "W", "X"], ["Y", "Z", "-", ".", "<", ">"], [" ", "&", "!", "?", "BYE", ""]]
+Players = ["        "] * 8
+xmax = len(letters[0]) - 1
+
+ymax = len(letters) - 1
+rectposxmin = letterMarginLeft - squareAlignement
+rectposxmax = letterMarginLeft + letterGap * xmax - squareAlignement
+rectposymin = letterMarginTop - squareAlignement
+rectposymax = letterMarginTop + letterGap * ymax - squareAlignement
+rectposxstep = letterGap
+rectposystep = letterGap
+
 
 # Player Name, Score box center coordinates (add approx. 3px to vertical center for uppercase letters)
 G1P1CXY = (95, 23)
@@ -72,33 +94,139 @@ MGSET2CXY = (465, 235)
 
 
 def text_objects(text, font):
-    textSurface = font.render(text, True, BLACK)
-    return textSurface, textSurface.get_rect()
+    textsurface = font.render(text, True, BLACK)
+    return textsurface, textsurface.get_rect()
 
 
-def text_blit(text, font, color, center):
-    textrender = font.render(text, True, color)
+def text_blit(text, font, clr, center):
+    textrender = font.render(text, True, clr)
     return textrender, textrender.get_rect(center=center)
 
 
+def select_player(event, index):
+    if event.type == pygame.KEYUP and event.key == pygame.K_DOWN:
+        index += 1
+        if index > 7:
+            index = 0
+    if event.type == pygame.KEYUP and event.key == pygame.K_UP:
+        index -= 1
+        if index < 0:
+            index = 7
+    return index
+
+
+def letterpicker(event, rectposx, rectposy, x, y):
+    if event.type == pygame.KEYUP and event.key == pygame.K_RIGHT:
+        rectposx += rectposxstep
+        x += 1
+        if rectposx > rectposxmax:
+            rectposx = rectposxmin
+            x = 0
+        # pygame.draw.rect(screen, ORANGE, [rectposx, rectposy, 20, 20], 2)
+    if event.type == pygame.KEYUP and event.key == pygame.K_LEFT:
+        rectposx -= rectposxstep
+        x -= 1
+        if rectposx < rectposxmin:
+            rectposx = rectposxmax
+            x = xmax
+        # pygame.draw.rect(screen, ORANGE, [rectposx, rectposy, 20, 20], 2)
+    if event.type == pygame.KEYUP and event.key == pygame.K_DOWN:
+        rectposy += rectposystep
+        y += 1
+        if rectposy > rectposymax:
+            rectposy = rectposymin
+            y = 0
+        # pygame.draw.rect(screen, ORANGE, [rectposx, rectposy, 20, 20], 2)
+    if event.type == pygame.KEYUP and event.key == pygame.K_UP:
+        rectposy -= rectposystep
+        y -= 1
+        if rectposy < rectposymin:
+            rectposy = rectposymax
+            y = ymax
+        # pygame.draw.rect(screen, ORANGE, [rectposx, rectposy, 20, 20], 2)
+    return rectposx, rectposy, x, y
+
+
 def init_players():
+    fontplayer = pygame.font.Font("OCRAStd.otf", 40)
+    rectposx = rectposxmin
+    rectposy = rectposymin
+    player_index = 0
+    player_x = 10
+    player_y = 50
+    player_vgap = 50
+    player_square = 30
+    name_x = 40
+    name_y = 45
+    name_gap = 29
+    name_square = 39
+    x = 0
+    # xmax = len(letters[0]) - 1
+    y = 0
+    # pygame.draw.rect(DISPLAYSURF, ORANGE, [rectposx, rectposy, squareSide, squareSide], 2)
+    initials = [" "] * 8
+    ii = 0
+
     init = True
+    player_picker = True
     while init:
+        background_image_player = pygame.image.load("TournamentScorerBGPlayer.png").convert()
+        DISPLAYSURF.blit(background_image_player, (0, 0))
+        if player_picker:
+            pygame.draw.rect(DISPLAYSURF, RED, [player_x, player_y + player_vgap * player_index,
+                                                player_square, player_square], 2)
+        else:
+            pygame.draw.rect(DISPLAYSURF, RED, [rectposx, rectposy, 50, 50], 2)
+            pygame.draw.rect(DISPLAYSURF, RED, [name_x + ii*name_gap, name_y + player_vgap * player_index,
+                                                name_square, name_square], 2)
+
+        # drawletters(DISPLAYSURF, letters)
+        # drawnumbers(DISPLAYSURF, numbers)
+        DISPLAYSURF.blit(fontplayer.render(Players[0], True, ORANGE), (45, 50))
+        DISPLAYSURF.blit(fontplayer.render(Players[1], True, ORANGE), (45, 100))
+        DISPLAYSURF.blit(fontplayer.render(Players[2], True, ORANGE), (45, 150))
+        DISPLAYSURF.blit(fontplayer.render(Players[3], True, ORANGE), (45, 200))
+        DISPLAYSURF.blit(fontplayer.render(Players[4], True, ORANGE), (45, 250))
+        DISPLAYSURF.blit(fontplayer.render(Players[5], True, ORANGE), (45, 300))
+        DISPLAYSURF.blit(fontplayer.render(Players[6], True, ORANGE), (45, 350))
+        DISPLAYSURF.blit(fontplayer.render(Players[7], True, ORANGE), (45, 400))
+        # DISPLAYSURF.blit(fontbig.render(initials[1], True, ORANGE), (230, 300))
+        # DISPLAYSURF.blit(fontbig.render(initials[2], True, ORANGE), (260, 300))
+        # DISPLAYSURF.blit(txt_surf, txt_rect)
+
         for event in pygame.event.get():
-            print(event)
+            # print(event)
             if event.type == pygame.QUIT or (event.type == KEYUP and event.key == K_ESCAPE):
                 init = False
-                # pygame.quit()
-                # quit()
-
-        DISPLAYSURF.fill(WHITE)
-        largeText = pygame.font.Font('freesansbold.ttf', 48)
-        TextSurf, TextRect = text_objects("Tournament Scoring Machine", largeText)
-        TextRect.center = ((400), (240))
-        DISPLAYSURF.blit(TextSurf, TextRect)
-
-
-
+            if player_picker:
+                if event.type == pygame.KEYUP and event.key in [pygame.K_UP, pygame.K_DOWN]:
+                    player_index = select_player(event, player_index)
+                if event.type == pygame.KEYUP and event.key == pygame.K_SPACE:
+                    player_picker = False
+                    initials = list(Players[player_index])
+                    ii = 0
+            else:
+                if event.type == pygame.KEYUP and \
+                        event.key in [pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT]:
+                    rectposx, rectposy, x, y = letterpicker(event, rectposx, rectposy, x, y)
+                    print(event)
+                    print(rectposx, rectposy, x, y)
+                if event.type == pygame.KEYUP and event.key in [pygame.K_RETURN, pygame.K_KP_ENTER]:
+                    print("Initials: ", "".join(initials))
+                    Players[player_index] = "".join(initials)
+                    print("Player:", Players[player_index])
+                    player_picker = True
+                    if player_index < 7:
+                        player_index += 1
+                    else:
+                        player_index = 0
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                    # print(letters[y][x])
+                    initials[ii] = letters[y][x]
+                    Players[player_index] = "".join(initials)
+                    ii += 1
+                    if ii > 7:
+                        ii = 0
 
         pygame.display.update()
 
@@ -109,14 +237,17 @@ def init_players():
     # Put names on Display
 
     # Return seeded players 1v8 vs 4v5 - 3v6 vs 2v7
-    plist = ["PLAYER 1", "PLAYER 8", "PLAYER 4", "PLAYER 5", "PLAYER 3", "PLAYER 6", "PLAYER 2", "PLAYER 7"]
+    # plist = ["PLAYER 1", "PLAYER 8", "PLAYER 4", "PLAYER 5", "PLAYER 3", "PLAYER 6", "PLAYER 2", "PLAYER 7"]
+    plist = [str(Players[0]).strip(), str(Players[7]).strip(), str(Players[3]).strip(), str(Players[4]).strip(),
+             str(Players[2]).strip(), str(Players[5]).strip(), str(Players[1]).strip(), str(Players[6]).strip()]
     print("Original list : ",  plist)
     if rnd == 1:
         random.shuffle(plist)  # shuffle method
         print("List after first shuffle  : ", plist)
     wlist = [""] * 6
     plist.extend(wlist)
-    return plist, [3] * 14
+    setlist = [3] * 8 + [5] * 4 + [7] * 2
+    return plist, setlist
 
 
 def init_sets():
@@ -125,13 +256,8 @@ def init_sets():
 
 
 def init_bracket(screen):
-    # screen.fill(GRAY)
-    # Initialize the joysticks
-    pygame.joystick.init()
-
     background_image = pygame.image.load("TournamentScorerBG.png").convert()
     screen.blit(background_image, (0, 0))
-    # player_box = pygame.image.load("PlayerBox.png").convert()
 
 
 def main():
@@ -142,7 +268,7 @@ def main():
     joystick.init()
     FPSCLOCK = pygame.time.Clock()
     DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
-
+    # DISPLAYSURF = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)  # For screen on Raspberry Pi
     pygame.display.set_caption('Simple Elimination Tournament')
 
     player_list, sets_list = init_players()
@@ -213,7 +339,7 @@ def main():
         #   scores: score list as they are entered
         for event in pygame.event.get():  # event handling loop
             if event.type == QUIT or (event.type == KEYUP and event.key == K_ESCAPE) or \
-                    (event.type == pygame.JOYBUTTONDOWN and joystick.get_button(8)):  # START
+                    (event.type == pygame.JOYBUTTONDOWN and joystick.get_button(9)):  # START
                 pygame.quit()
                 sys.exit()
 
@@ -232,7 +358,7 @@ def main():
                     currentgame -= 1
 
             if (event.type == KEYUP and event.key == K_x) or \
-                    (event.type == pygame.JOYBUTTONDOWN and joystick.get_button(9)):  # SELECT
+                    (event.type == pygame.JOYBUTTONDOWN and joystick.get_button(8)):  # SELECT
                 if correction:
                     correction = False
                 else:
@@ -263,6 +389,8 @@ def main():
                     print(player_list[currentgame*2-2], ' wins')
                     if currentgame != 7:
                         player_list[currentgame+7] = player_list[currentgame*2-2]
+                    # else:
+                        # display_winner()
 
                 elif sets_list[currentgame*2-1] == scores[currentgame*2-1]:
                     winner_list[currentgame*2-2] = False
@@ -270,6 +398,8 @@ def main():
                     print(player_list[currentgame*2-1], ' wins')
                     if currentgame != 7:
                         player_list[currentgame+7] = player_list[currentgame*2-1]
+                    # else:
+                        # display_winner()
                 else:
                     winner_list[currentgame * 2 - 2] = False
                     winner_list[currentgame * 2 - 1] = False
